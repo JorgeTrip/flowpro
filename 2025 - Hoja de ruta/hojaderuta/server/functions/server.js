25d8ebde376a485f8d2175eb3ce117de24f58d83
Jorge O. Tripodi <jorgeotripodi@hotmail.com>
2025-08-21 00:03:16 -0300

    chore: untrack directories as per .gitignore

--- 2025 - Hoja de ruta/hojaderuta/server/functions/server.js
+++
@@ -1,7 +0,0 @@
-// server/functions/server.js
-const express = require('express');
-const serverless = require('serverless-http');
-const app = require('../server'); // Importa tu aplicación Express
-
-// Exporta la aplicación como una función serverless
-module.exports.handler = serverless(app);
