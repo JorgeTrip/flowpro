c3fd9ec5304c23aeca422397806a607b3fe4135e
Jorge O. Tripodi <jorgeotripodi@hotmail.com>
2025-08-22 20:34:31 -0300

    Corrijo errores que impiden build en netlify

--- ventas/src/components/Dashboard/index.ts
+++
@@ -1,8 +0,0 @@
-// Exportar todos los componentes del dashboard
-export { default as VentasMensualesChart } from './VentasMensualesChart';
-export { default as VentasPorRubroChart } from './VentasPorRubroChart';
-export { default as VentasPorZonaChart } from './VentasPorZonaChart';
-export { default as TopProductosChart } from './TopProductosChart';
-export { default as TopClientesChart } from './TopClientesChart';
-export { default as VendedoresChart } from './VendedoresChart';
-export { default as TopProductosPorCategoriaChart } from './TopProductosPorCategoriaChart';
